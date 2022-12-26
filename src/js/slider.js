7af562501770fceb81e1a9fcdafd801f468430d8
Andrei L <davinchi.l.a.s@mail.ru>
2022-12-26 17:49:12 +0300

    section-6,7,8,9,footer

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,155 +1,241 @@
-// Slider(all Slides in a container)
-const slider = document.querySelector(".slider")
-// All trails 
-const trail = document.querySelector(".trail").querySelectorAll("div")
+"use strict"
 
-// Transform value
-let value = 0
-// trail index number
-let trailValue = 0
-// interval (Duration)
-let interval = 4000000
+function Sim(slidd) {
 
-// Function to slide forward
-const slide = (condition) => {
-    // CLear interval
-    clearInterval(start)
-    // update value and trailValue
-    condition === "increase" ? initiateINC() : initiateDEC()
-    // move slide
-    move(value, trailValue)
-    // Restart Animation
-    animate()
-    // start interal for slides back 
-    start = setInterval(() => slide("increase"), interval);
-}
+	let id = document.getElementById(slidd);
+	if(id) {
+		this.sldrRoot = id
+	}
+	else {
+		this.sldrRoot = document.querySelector('.sim-slider')
+	};
 
-// function for increase(forward, next) configuration
-const initiateINC = () => {
-    // Remove active from all trails
-    trail.forEach(cur => cur.classList.remove("active"))
-    // increase transform value
-    value === 50 ? value = 0 : value += 50
-    // update trailValue based on value
-    trailUpdate()
-}
-
-// function for decrease(backward, previous) configuration
-const initiateDEC = () => {
-     // Remove active from all trails
-    trail.forEach(cur => cur.classList.remove("active"))
-    // decrease transform value
-    value === 0 ? value = 50 : value -= 50
-     // update trailValue based on value
-    trailUpdate()
-}
-
-// function to transform slide 
-const move = (S, T) => {
-    // transform slider
-    slider.style.transform = `translateX(-${S}%)`
-    //add active class to the current trail
-    trail[T].classList.add("active")
-}
-
-const tl = gsap.timeline({defaults: {duration: 0.6, ease: "power2.inOut"}})
-tl.from(".slide-text", {opacity: 0, y: "30px"}, "-=0.3")
-tl.from(".slide-title", {opacity: 0, y: "30px"}, "-=0.3")
+	// Carousel objects
+	this.sldrList = this.sldrRoot.querySelector('.sim-slider-list');
+	this.sldrElements = this.sldrList.querySelectorAll('.sim-slider-element');
+	this.sldrElemFirst = this.sldrList.querySelector('.sim-slider-element');
+	this.leftArrow = this.sldrRoot.querySelector('div.sim-slider-arrow-left');
+	this.rightArrow = this.sldrRoot.querySelector('div.sim-slider-arrow-right');
 
 
-// function to restart animation
-const animate = () => tl.restart()
+	// Initialization
+	this.options = Sim.defaults;
+	Sim.initialize(this)
+};
 
-// function to update trailValue based on slide value
-const trailUpdate = () => {
-    if (value === 0) {
-        trailValue = 0
-    } else if (value === 20) {
-        trailValue = 1
-    } else if (value === 40) {
-        trailValue = 2
-    } else if (value === 60) {
-        trailValue = 3
-    } else {
-        trailValue = 4
-    }
-}   
+Sim.defaults = {
 
-// Start interval for slides
-let start = setInterval(() => slide("increase"), interval)
+	// Default options for the carousel
+	loop: true,     // Бесконечное зацикливание слайдера
+	interval: 50000, // Интервал между пролистыванием элементов (мс)
+	arrows: true,   // Пролистывание стрелками
 
-// Next  and  Previous button function (SVG icon with different classes)
-document.querySelectorAll("svg").forEach(cur => {
-    // Assign function based on the class Name("next" and "prev")
-    cur.addEventListener("click", () => cur.classList.contains("next") ? slide("increase") : slide("decrease"))
-})
+};
 
-// function to slide when trail is clicked
-const clickCheck = (e) => {
-    // CLear interval
-    clearInterval(start)
-    // remove active class from all trails
-    trail.forEach(cur => cur.classList.remove("active"))
-    // Get selected trail
-    const check = e.target
-    // add active class
-    check.classList.add("active")
+Sim.prototype.elemPrev = function(num) {
+	num = num || 1;
 
-    // Update slide value based on the selected trail
-    if(check.classList.contains("box1")) {
-        value = 0
-    } else if (check.classList.contains("box2")) {
-        value = 20
-    } else if (check.classList.contains("box3")) {
-        value = 40
-    } else if (check.classList.contains("box4")) {
-        value = 60
-    } else {
-        value = 80
-    }
-    // update trail based on value
-    trailUpdate()
-    // transfrom slide
-    move(value, trailValue)
-    // start animation
-    animate()
-    // start interval
-    start = setInterval(() => slide("increase"), interval)
-}
+	let prevElement = this.currentElement;
+	this.currentElement -= num;
+	if(this.currentElement < 0) this.currentElement = this.elemCount-1;
 
-// Add function to all trails
-trail.forEach(cur => cur.addEventListener("click", (ev) => clickCheck(ev)))
+	if(!this.options.loop) {
+		if(this.currentElement == 0) {
+			this.leftArrow.style.display = 'none'
+		};
+		this.rightArrow.style.display = 'block'
+	};
+	
+	this.sldrElements[this.currentElement].style.opacity = '1';
+	this.sldrElements[prevElement].style.opacity = '0';
+};
 
-// Mobile touch Slide Section
-const touchSlide = (() => {
-    let start, move, change, sliderWidth
+Sim.prototype.elemNext = function(num) {
+	num = num || 1;
+	
+	let prevElement = this.currentElement;
+	this.currentElement += num;
+	if(this.currentElement >= this.elemCount) this.currentElement = 0;
 
-    // Do this on initial touch on screen
-    slider.addEventListener("touchstart", (e) => {
-        // get the touche position of X on the screen
-        start = e.touches[0].clientX
-        // (each slide with) the width of the slider container divided by the number of slides
-        sliderWidth = slider.clientWidth/trail.length
-    })
-    
-    // Do this on touchDrag on screen
-    slider.addEventListener("touchmove", (e) => {
-        // prevent default function
-        e.preventDefault()
-        // get the touche position of X on the screen when dragging stops
-        move = e.touches[0].clientX
-        // Subtract initial position from end position and save to change variabla
-        change = start - move
-    })
+	if(!this.options.loop) {
+		if(this.currentElement == this.elemCount-1) {
+			this.rightArrow.style.display = 'none'
+		};
+		this.leftArrow.style.display = 'block'
+	};
 
-    const mobile = (e) => {
-        // if change is greater than a quarter of sliderWidth, next else Do NOTHING
-        change > (sliderWidth/4)  ? slide("increase") : null;
-        // if change * -1 is greater than a quarter of sliderWidth, prev else Do NOTHING
-        (change * -1) > (sliderWidth/4) ? slide("decrease") : null;
-        // reset all variable to 0
-        [start, move, change, sliderWidth] = [0,0,0,0]
-    }
-    // call mobile on touch end
-    slider.addEventListener("touchend", mobile)
-})()
+	this.sldrElements[this.currentElement].style.opacity = '1';
+	this.sldrElements[prevElement].style.opacity = '0';
+
+};
+
+
+Sim.initialize = function(that) {
+
+	// Constants
+	that.elemCount = that.sldrElements.length; // Количество элементов
+
+	// Variables
+	that.currentElement = 0;
+	let bgTime = getTime();
+
+	// Functions
+	function getTime() {
+		return new Date().getTime();
+	};
+
+
+	// Start initialization
+	if(that.elemCount <= 1) {   // Отключить навигацию
+		that.options.arrows = false;
+		that.leftArrow.style.display = 'none'; that.rightArrow.style.display = 'none'
+	};
+	if(that.elemCount >= 1) {   // показать первый элемент
+		that.sldrElemFirst.style.opacity = '1';
+	};
+
+
+	if(that.options.arrows) {  // инициализация стрелок
+		that.leftArrow.addEventListener('click', function() {
+			let fnTime = getTime();
+			if(fnTime - bgTime > 1000) {
+				bgTime = fnTime; that.elemPrev()
+			}
+		}, false);
+		that.rightArrow.addEventListener('click', function() {
+			let fnTime = getTime();
+			if(fnTime - bgTime > 1000) {
+				bgTime = fnTime; that.elemNext()
+			}
+		}, false)
+	}
+	else {
+		that.leftArrow.style.display = 'none'; that.rightArrow.style.display = 'none'
+	};
+
+
+};
+
+new Sim();
+
+
+function Simq(slid) {
+
+	let id = document.getElementById(slid);
+		this.sldrRoot = id;
+        console.log(id);
+
+
+	// Carousel objects
+	this.sldrList = this.sldrRoot.querySelector('.sim-slider-listq');
+	this.sldrElements = this.sldrList.querySelectorAll('.sim-slider-elementq');
+	this.sldrElemFirst = this.sldrList.querySelector('sim-slider-elementq');
+	this.leftArrow = this.sldrRoot.querySelector('div.sim-slider-arrow-leftq');
+	this.rightArrow = this.sldrRoot.querySelector('div.sim-slider-arrow-rightq');
+
+
+	// Initialization
+	this.options = Sim.defaults;
+	Sim.initialize(this)
+};
+
+Sim.defaults = {
+
+	// Default options for the carousel
+	loop: true,     // Бесконечное зацикливание слайдера
+	interval: 50000, // Интервал между пролистыванием элементов (мс)
+	arrows: true,   // Пролистывание стрелками
+
+};
+
+Sim.prototype.elemPrev = function elemPrev(num) {
+	num = num || 1;
+
+	let prevElement = this.currentElement;
+	this.currentElement -= num;
+	if(this.currentElement < 0) this.currentElement = this.elemCount-1;
+
+	if(!this.options.loop) {
+		if(this.currentElement == 0) {
+			this.leftArrow.style.display = 'none'
+		};
+		this.rightArrow.style.display = 'block'
+	};
+	
+	this.sldrElements[this.currentElement].style.opacity = '1';
+	this.sldrElements[prevElement].style.opacity = '0';
+};
+
+Sim.prototype.elemNext = function elemNext(num) {
+	num = num || 1;
+	
+	let prevElement = this.currentElement;
+	this.currentElement += num;
+	if(this.currentElement >= this.elemCount) this.currentElement = 0;
+
+	if(!this.options.loop) {
+		if(this.currentElement == this.elemCount-1) {
+			this.rightArrow.style.display = 'none'
+		};
+		this.leftArrow.style.display = 'block'
+	};
+
+	this.sldrElements[this.currentElement].style.opacity = '1';
+	this.sldrElements[prevElement].style.opacity = '0';
+
+};
+
+
+Sim.initialize = function(that) {
+
+	// Constants
+	that.elemCount = that.sldrElements.length; // Количество элементов
+
+	// Variables
+	that.currentElement = 0;
+	let bgTime = getTime();
+
+	// Functions
+	function getTime() {
+		return new Date().getTime();
+	};
+
+
+	// Start initialization
+	if(that.elemCount <= 1) {   // Отключить навигацию
+		that.options.arrows = false;
+		that.leftArrow.style.display = 'none'; that.rightArrow.style.display = 'none'
+	};
+	if(that.elemCount >= 1) {   // показать первый элемент
+		that.sldrElemFirst.style.opacity = '1';
+	};
+
+
+	if(that.options.arrows) {  // инициализация стрелок
+		that.leftArrow.addEventListener('click', function() {
+			let fnTime = getTime();
+			if(fnTime - bgTime > 1000) {
+				bgTime = fnTime;
+                elemPrev();
+			}
+		}, false);
+		that.rightArrow.addEventListener('click', function() {
+			let fnTime = getTime();
+			if(fnTime - bgTime > 1000) {
+				bgTime = fnTime; 
+                elemNext()
+			}
+		}, false)
+	}
+	else {
+		that.leftArrow.style.display = 'none'; that.rightArrow.style.display = 'none'
+	};
+
+
+};
+
+new Simq();
+
+
+
+
